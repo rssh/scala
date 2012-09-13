188ce468bd0dff80dada4d9e896a8398993900f1
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2012-09-14 02:41:33 +0300

    added exported annotation

---
+++ src/library/scala/annotation/exported.scala
@@ -0,0 +1,16 @@
+package scala.annotation
+
+/**
+ * import annotation which used to mark import statement
+ * be exported to object, which import scope of statement definition.
+ * Usage:
+{{{
+ package object dependencies {
+    @exported import com.library1._
+    @exported import com.library2._
+ }
+}}}
+ *
+ * @since 2.11
+ */
+final class exported extends annotation.StaticAnnotation
